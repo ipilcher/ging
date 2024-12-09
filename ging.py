d2935b67f40cd9e68e57a949f586af629c22082f
Ian Pilcher <arequipeno@gmail.com>
2024-12-09 11:23:07 -0600

    Miscellaneous cleanups
    * Add SPDX license comment
    * Add copyright notice
    * Remove unneeded logging
    * Triple-space between functions
    * Add kate formatting comment

--- ging.py
+++ ging.py
@@ -1,3 +1,11 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+#
+#   GING - The Google ID Number Getter
+#
+#   Copyright 2024 Ian Pilcher <arequipeno@gmail.com>
+#
+
 import os
 import json
 import datetime
@@ -30,6 +38,7 @@ app = flask.Flask(__name__)
 with open('/tmp/ging-session-key', 'rb') as gsk:
     app.secret_key = gsk.read()
 
+
 def get_oauth_flow():
     if not (flow := getattr(get_oauth_flow, '_flow', None)):
         url = flask.url_for('oauth2callback', _external=True)
@@ -52,6 +61,7 @@ def hello_world():
     response.mimetype = 'text/plain'
     return response
 
+
 @app.route('/login')
 def login():
     if 'oauth_creds' not in flask.session:
@@ -59,26 +69,19 @@ def login():
     else:
         return flask.redirect('success')
 
+
 @app.route('/auth')
 def auth():
-    #app.logger.debug(f'/auth: Secret key is {app.secret_key}')
     auth_url, state = get_oauth_flow().authorization_url()
     response = flask.make_response(flask.redirect(auth_url))
-    #app.logger.debug(f'Setting session state: {state}')
     flask.session['state'] = state
-    # This doesn't show the session cookie
-    # app.logger.debug(f'Response headers: {response.headers}')
     return response
+
 
 @app.route('/oauth2callback')
 def oauth2callback():
-    #app.logger.debug(f'/oauth2callback: Secret key is {app.secret_key}')
-    #app.logger.debug(f'Cookie headers: {flask.request.headers.get("Cookie")}')
     session_state = flask.session.get('state')
-    #app.logger.debug(f'Got session state: {session_state}')
     request_state = flask.request.args.get('state')
-    #app.logger.debug(f'Got request state: {request_state}')
-    # Disabling this check, because it doesn't work
     if session_state is None or session_state != request_state:
         return 'Danger, Will Robinson!', 400
     response = flask.make_response(flask.redirect('success'))
@@ -88,12 +91,11 @@ def oauth2callback():
     flask.session['oauth_creds'] = flow.credentials.to_json()
     return response
 
+
 @app.route('/success')
 def success():
-    #app.logger.debug(f'/success: Secret key is {app.secret_key}')
     if not (creds_json := flask.session.get('oauth_creds')):
         return "You don't have credentials!", 400
-    #app.logger.debug(f'Got credentials: {creds_json}')
     creds_dict = json.loads(creds_json)
     app.logger.debug(f'expiry: {creds_dict['expiry']}')
     expiry = datetime.datetime.fromisoformat(creds_dict['expiry'])
@@ -107,9 +109,9 @@ def success():
     response = flask.make_response(profile)
     response.mimetype = 'application/json'
     return response
-    #response = flask.make_response('Great success!')
-    #response.mimetype = 'text/plain'
-    #return response
+
 
 if __name__ == '__main__':
         app.run()
+
+# kate: indent-width 4; replace-tabs on;
